ab70868281ea65a256348ca52dc834d14bd4ef80
8wichmanndavid <48061083+8wichmanndavid@users.noreply.github.com>
2020-06-14 23:25:34 -0400

    Fixed Database and Added Expiration

---
+++ Expiration.py
@@ -0,0 +1,27 @@
+import mysql.connector
+
+def expiration(mycursor):
+    sqlvar = "SELECT\
+                PROD_NAME,\
+                BRAND,\
+                DEPT_NAME,\
+                QUANTITY,\
+                UNIT_PRC,\
+                EXPIR_DATE,\
+                PROD.SKU\
+                FROM PRODUCTS AS PROD\
+                JOIN INVENTORY AS INV\
+                ON PROD.SKU = INV.SKU\
+                JOIN DEPARTMENT AS DEPT\
+                ON DEPT.DEPT_NUM = PROD.DEPT_NUM\
+                ORDER BY\
+                EXPIR_DATE,\
+                DEPT_NAME, \
+                PROD_NAME ,\
+                QUANTITY,\
+                BRAND ,\
+                UNIT_PRC, \
+                PROD.SKU"
+                     
+    mycursor.execute(sqlvar)
+    result = mycursor.fetchall()
