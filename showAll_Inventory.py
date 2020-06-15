748fa4aa25859dd04e6bdb134e8a018224a9b625
14erHiker <48064317+14erHiker@users.noreply.github.com>
2020-06-14 20:57:33 -0500

    Add files via upload

---
+++ showAll_Inventory.py
@@ -0,0 +1,29 @@
+import mysql.connector
+
+def showAll_Inventory():
+    sqlvar = "SELECT PRODUCTS.PROD_NAME, PRODUCTS.BRAND, DEPARTMENT.DEPT_NAME, SUM(INVENTORY.QUANTITY), PRODUCTS.UNIT_PRC, INVENTORY.SKU \
+                     FROM DEPARTMENT \
+                     JOIN PRODUCTS \
+                     ON \
+                     PRODUCTS.DEPT_NUM = DEPARTMENT.DEPT_NUM \
+                     JOIN INVENTORY \
+                     ON \
+                     INVENTORY.SKU = PRODUCTS.SKU \
+                     GROUP BY \
+                     DEPARTMENT.DEPT_NAME, \
+                     PRODUCTS.PROD_NAME, \
+                     PRODUCTS.BRAND, \
+                     PRODUCTS.UNIT_PRC, \
+                     INVENTORY.SKU"
+                     
+    mycursor.execute(sqlvar)
+    result = mycursor.fetchall()
+
+    return result
+                 
+                 
+                 
+                 
+                
+                 
+                 
